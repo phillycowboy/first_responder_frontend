b53294dba6c1b7fc15b254d56d9c0f8541c03457
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-07-20 18:36:12 -0400

    patients now have their own individual show page

--- first-responder-app/src/PatientShow.js
+++ first-responder-app/src/PatientShow.js
@@ -4,11 +4,21 @@ import { connect } from 'react-redux'
 
 const PatientShow = (props) => {
     console.log("PatientShow", props)
-    const patient = props.patients.find((patient) => patient.id === props.match.params.id)
-    // console.log("this is the patient from Patient show", this.patient)
+    const patient = props.patients.find((patient) => patient.id === parseInt(props.match.params.id, 10))
+    console.log("this is the patient from Patient show", patient)
     return (
         <div>
-            {patient}
+            <h1>{patient.first_name} {patient.last_name}</h1>
+            <h3>Gender:{patient.gender} Pronouns:{patient.pronouns}</h3>
+            <h4>Heart Rate: {patient.heart_rate}</h4>
+            <h4>Blood Pressure: {patient.blood_pressure}</h4>
+            <h4>Respiration Rate: {patient.respiration_rate}</h4>
+            <h4>Temp: {patient.temperature}</h4>
+            <h4>Blood Glucose Level: {patient.blood_glucose_level}</h4>
+            <h4>End Tidal: {patient.end_tidal}</h4>
+            <h4>Chief Complaint: {patient.chief_complaint}</h4>
+            <h4>On Scene Description: {patient.on_scene_description}</h4>
+            <h4>Currently Admitted At: {patient.hospital.name}</h4>
             {/* {props.patients.map((p) => (
                 p.first_name
             ))} */}
