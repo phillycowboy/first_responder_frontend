fae0b98791a5e00c112e12514f1ac34ed0934434
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-07-21 12:46:41 -0400

    impemented timer to count up just cant get it to stop also when you redirect to a page it resets

--- first-responder-app/src/components/Timer.js
+++ first-responder-app/src/components/Timer.js
@@ -2,21 +2,30 @@ import React, {Component} from 'react'
 
 class Timer extends Component{
     state = {
-        hour: 0,
         minute: 0,
         seconds: 0
     }
 
     handleStartTime = () => {
         console.log("This should START the timer")
+        setInterval(() => {
+            this.setState({
+                seconds: this.state.seconds === 59 ? 0 : this.state.seconds + 1,
+                minute: this.state.seconds === 59 ? this.state.minute + 1 : this.state.minute
+            })
+        }, 1000)
     }
+   
     handleStopTime = () => {
         console.log("This should STOP the timer")
+        clearInterval(this.state) 
+  
     }
 
     render(){
         return(
             <div>
+                <div>Patient Wait Time: {this.state.minute}mins : {this.state.seconds}secs</div> <br />
                 <button onClick={this.handleStartTime}>Patient Arrived</button>  <button onClick={this.handleStopTime}>Patient Seen</button>
             </div>
         )
